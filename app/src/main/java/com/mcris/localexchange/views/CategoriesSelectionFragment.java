774d395211f9c6f90c804d897259c3f661fa913a
Massimiliano Cristarella <m.cristarella@outlook.com>
2022-04-26 23:36:24 +0200

    Implemented search by text and category

--- app/src/main/java/com/mcris/localexchange/views/CategoriesSelectionFragment.java
+++ app/src/main/java/com/mcris/localexchange/views/CategoriesSelectionFragment.java
@@ -1,9 +1,12 @@
 package com.mcris.localexchange.views;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,6 +14,7 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.mcris.localexchange.databinding.FragmentCategoriesSelectionBinding;
 import com.mcris.localexchange.models.CategoriesAdapter;
 import com.mcris.localexchange.models.ClickableAdapterListener;
@@ -24,6 +28,8 @@ public class CategoriesSelectionFragment extends Fragment {
 
     private FragmentCategoriesSelectionBinding binding;
     private MainViewModel mainViewModel;
+
+    private CategoriesAdapter categoriesAdapter;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -42,17 +48,53 @@ public class CategoriesSelectionFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        List<Category> categories = mainViewModel.getCategories();
-        CategoriesAdapter adapter = new CategoriesAdapter(categories);
-        adapter.setOnClickListener(new ClickableAdapterListener<Category>() {
+        List<Category> categories = mainViewModel.getAllCategories();
+        categoriesAdapter = new CategoriesAdapter(categories);
+        categoriesAdapter.setOnClickListener(new ClickableAdapterListener<Category>() {
             @Override
             public void onListItemClick(Category item, int position) {
 
             }
         });
 
+        binding.searchEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    search();
+                    v.clearFocus();
+                    if (getActivity() instanceof MainActivity) {
+                        ((MainActivity) getActivity()).hideSoftKeyboard(v);
+                    }
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        binding.searchItemsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.searchEditText.clearFocus();
+                search();
+            }
+        });
         binding.categoriesRecyclerView.setHasFixedSize(true);
-        binding.categoriesRecyclerView.setAdapter(adapter);
+        binding.categoriesRecyclerView.setAdapter(categoriesAdapter);
         binding.categoriesRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+    }
+
+    public void search() {
+        String text = binding.searchEditText.getText().toString().trim();
+        if (text.length() == 0 || text.length() > 2) {
+            mainViewModel.getObservableItems().clear();
+            mainViewModel.setSearchText(text);
+            Category c = categoriesAdapter.getSelectedCategory();
+            mainViewModel.setSelectedCategoryId(c != null ? c.getId() : null);
+            mainViewModel.downloadItems();
+        }
+        if (getActivity() instanceof MainActivity) {
+            ((MainActivity) getActivity()).setSheetBehaviorState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
     }
 }
