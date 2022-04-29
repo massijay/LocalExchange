74e2298e229d5439957b2f607163d4675c468ef4
Massimiliano Cristarella <m.cristarella@outlook.com>
2022-04-29 20:27:24 +0200

    Added item details fragment (only UI)

---
+++ app/src/main/java/com/mcris/localexchange/views/ItemDetailsFragment.java
@@ -0,0 +1,38 @@
+package com.mcris.localexchange.views;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mcris.localexchange.databinding.FragmentItemDetailsBinding;
+import com.mcris.localexchange.viewmodels.MainViewModel;
+
+public class ItemDetailsFragment extends Fragment {
+
+    private FragmentItemDetailsBinding binding;
+    private MainViewModel mainViewModel;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mainViewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentItemDetailsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
